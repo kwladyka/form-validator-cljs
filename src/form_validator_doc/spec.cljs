ed61a48982b379fa224d073082135b47a9c5d118
Krzysztof Władyka <krzysztof@wladyka.eu>
2019-10-24 17:02:16 +0200

    define :form validators

--- src/form_validator_doc/spec.cljs
+++ src/form_validator_doc/spec.cljs
@@ -2,11 +2,25 @@
   (:require [cljs.spec.alpha :as s]
             [clojure.test.check.generators]))
 
+(s/def ::checked boolean)
+(s/def ::selected not-empty)
+
 (s/def ::email (s/and string? (partial re-matches #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")))
 
 (s/def ::password-not-empty not-empty)
 (s/def ::password-length #(<= 6 (count %)))
 (s/def ::password (s/and string? ::password-not-empty ::password-length))
 
-(s/def ::form (s/keys :req-un [::email ::password]
+(s/def ::checkbox-with-value (s/and ::checked))
+(s/def ::checkbox-without-value (s/and ::checked))
+
+(s/def ::select-one (s/and ::selected #{"green"}))
+(s/def ::select-multiple (partial some #{"cat"}))
+
+(s/def ::radio #{"red"})
+
+(s/def ::form (s/keys :req-un [::email ::password
+                               ::checkbox-with-value ::checkbox-without-value
+                               ::select-one ::select-multiple
+                               ::radio]
                       :opt-un [::password-repeat]))
